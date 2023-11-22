91f381d9e9479e61ae52ed83eca741f7a3ca5030
Tanjul <sarathetanjul@gmail.com>
2023-11-22 17:54:10 +0530

    first commit

---
+++ client/src/components/TeamMessage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MessageTeam, useMessageContext } from 'stream-chat-react';
+
+const TeamMessage = () => {
+    const { message } = useMessageContext();
+
+    return (
+        <MessageTeam
+            message={{ ...message, user: {}}}
+        />
+    )
+}
+
+export default TeamMessage
